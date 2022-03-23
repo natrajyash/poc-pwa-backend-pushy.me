fe60fec1d96b5d2b1224871f0c51e5fd40baa7d6
Natraj S <natraj.s@yash.com>
2022-03-23 14:13:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
+const webpush = require('web-push');
+
+const vapidKeys = {
+    "publicKey": "BJy_ehmZZv-Kh2BZgVjvhtEtYq0ukfoG3M_ujLcR84_eLsvf6DifhDbwqrM3Xl5eRXzeQ8SJPpxoP0OFwf_fbxA",
+    "privateKey": "T-EEBeiogCNG1xftNWqA6oF1-RgHBq4d0678of_c4Pg"
+};
+
+
+webpush.setVapidDetails(
+    'mailto:example@yourdomain.org',
+    vapidKeys.publicKey,
+    vapidKeys.privateKey
+);
+
+const addPushSubscriber = require("./add-push-subscriber.route");
+const sendNewsletter = require("./send-newsletter.route");
+
+const port = 3000;
+const html = __dirname + '/poc-pwa/';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.route('/api/notifications')
+    .post(addPushSubscriber);
+
+app.route('/api/newsletter')
+    .post(sendNewsletter);
+
+app.use(express.static('poc-pwa/'))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(html + 'index.html');
+// });
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
