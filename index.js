e28726d892996c61a69065371a135c60787cb10a
Natraj S <natraj.s@yash.com>
2022-03-23 19:42:37 +0530

    gitignore file and heroku setup

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
 const webpush = require('web-push');
 
 const vapidKeys = {
     "publicKey": "BJy_ehmZZv-Kh2BZgVjvhtEtYq0ukfoG3M_ujLcR84_eLsvf6DifhDbwqrM3Xl5eRXzeQ8SJPpxoP0OFwf_fbxA",
     "privateKey": "T-EEBeiogCNG1xftNWqA6oF1-RgHBq4d0678of_c4Pg"
 };
-
 
 webpush.setVapidDetails(
     'mailto:natraj.s@yash.com',
@@ -34,7 +32,6 @@ app.route('/api/newsletter')
     .post(sendNewsletter);
 
 app.use(express.static('poc-pwa/'));
-
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
