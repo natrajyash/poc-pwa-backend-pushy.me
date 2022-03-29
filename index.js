fb066ee997b0aff0103c32277812c3d2c941adda
Natraj S <natraj.s@yash.com>
2022-03-29 17:42:34 +0530

    Removing references to web-push package

--- index.js
+++ index.js
@@ -2,19 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const webpush = require('web-push');
-
-const vapidKeys = {
-    "publicKey": "BJy_ehmZZv-Kh2BZgVjvhtEtYq0ukfoG3M_ujLcR84_eLsvf6DifhDbwqrM3Xl5eRXzeQ8SJPpxoP0OFwf_fbxA",
-    "privateKey": "T-EEBeiogCNG1xftNWqA6oF1-RgHBq4d0678of_c4Pg"
-};
-
-webpush.setVapidDetails(
-    'mailto:natraj.s@yash.com',
-    vapidKeys.publicKey,
-    vapidKeys.privateKey
-);
-
 const addPushSubscriber = require("./add-push-subscriber.route");
 const sendNewsletter = require("./send-newsletter.route");
 
