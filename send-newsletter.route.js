c7e09135b10d2eab5ec2d6b1ce7e961054329ed0
Natraj S <natraj.s@yash.com>
2022-03-29 17:36:46 +0530

    Implementation of pushy.me

--- send-newsletter.route.js
+++ send-newsletter.route.js
@@ -1,37 +1,43 @@
 const USER_SUBSCRIPTIONS = require("./in-memory-db");
 
-const webpush = require('web-push');
+const Pushy = require('pushy');
+const pushyAPI = new Pushy('c400b2eaa89ea7808db844b5f1ff06b872744cbd4b562cf65fa8d220993fb6b8');
 
 module.exports = function sendNewsletter(req, res) {
 
     console.log('Total subscriptions', USER_SUBSCRIPTIONS.length);
 
-    // sample notification payload
-    const notificationPayload = {
-        "notification": {
-            "title": "POC-PWA News",
-            "body": "Newsletter Available!",
-            "icon": "/assets/icons/icon-128x128.png",
-            "vibrate": [100, 50, 100],
-            "data": {
-                "dateOfArrival": Date.now(),
-                "primaryKey": 1,
-                "onActionClick": {
-                    "default": {"operation": "openWindow"},
-                  }
+    if (USER_SUBSCRIPTIONS.length > 0) {
+        // Set push payload data to deliver to device(s) 
+        const data = {
+            message: 'New Newsletter!'
+        };
+    
+        // Set optional push notification options (such as iOS notification fields)
+        const options = {
+            notification: {
+                badge: 1,
+                sound: 'ping.aiff',
+                title: 'Test Newsletter',
+                body: 'Hello World \u270c',
             },
-            "actions": [{
-                "action": "explore",
-                "title": "Go to the site"
-            }]
-        }
-    };
-
-    Promise.all(USER_SUBSCRIPTIONS.map(sub => webpush.sendNotification(
-        sub, JSON.stringify(notificationPayload) )))
-        .then(() => res.status(200).json({message: 'Newsletter sent successfully.'}))
-        .catch(err => {
-            console.error("Error sending notification, reason: ", err);
-            res.sendStatus(500);
+        };
+    
+        // Send push notification via the Send Notifications API 
+        // https://pushy.me/docs/api/send-notifications 
+        pushyAPI.sendPushNotification(data, USER_SUBSCRIPTIONS.map(sub => sub.deviceToken), options, function (err, id) {
+            // Log errors to console 
+            if (err) {
+                console.log('Fatal Error', err);
+                return res.sendStatus(500);
+            }
+    
+            // Log success 
+            console.log('Push sent successfully! (ID: ' + id + ')');
+            return res.status(200).json({message: 'Notifications pushed successfully.'})
         });
+    } else {
+        return res.status(200).json({message: 'No Devices are registered!'})
+    }
+
 }
